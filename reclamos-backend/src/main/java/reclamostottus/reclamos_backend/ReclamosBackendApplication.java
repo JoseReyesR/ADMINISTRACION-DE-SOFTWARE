488479a9d9a7b54b6c58ed2e4df62a0da5de0e4c
JoseReyesR <Josereyesrubio95@gmail.com>
2026-09-10 00:21:17 -0500

    creacion de front y back

---
+++ reclamos-backend/src/main/java/reclamostottus/reclamos_backend/ReclamosBackendApplication.java
@@ -0,0 +1,13 @@
+package reclamostottus.reclamos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReclamosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReclamosBackendApplication.class, args);
+	}
+
+}
