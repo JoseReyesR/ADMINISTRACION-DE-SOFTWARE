038c4cc696e2177bfd17cfb5190e71497b23b53e
JoseReyesR <Josereyesrubio95@gmail.com>
2026-09-10 22:43:43 -0500

    creacion del DTO para conectar el frontyback

---
+++ reclamos-backend/src/main/java/reclamostottus/reclamos_backend/dto/ReclamoRequestDTO.java
@@ -0,0 +1,129 @@
+package reclamostottus.reclamos_backend.dto;
+
+public class ReclamoRequestDTO {
+
+    // --- Datos del Paso 1 (Cliente) ---
+    private String tipoDocumento;
+    private String numeroDocumento;
+    private String nombres;
+    private String apellidos;
+    private String correo;
+    private String celular;
+
+    // --- Datos del Paso 2 (Incidente) ---
+    private String tipoSolicitud;
+    private String canalCompra;
+    private String tienda;
+    private String numeroBoleta;
+    private String motivo;
+    private String producto;
+    private String descripcion;
+
+    public ReclamoRequestDTO() {
+    }
+
+    // Getters y Setters
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(String numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getTipoSolicitud() {
+        return tipoSolicitud;
+    }
+
+    public void setTipoSolicitud(String tipoSolicitud) {
+        this.tipoSolicitud = tipoSolicitud;
+    }
+
+    public String getCanalCompra() {
+        return canalCompra;
+    }
+
+    public void setCanalCompra(String canalCompra) {
+        this.canalCompra = canalCompra;
+    }
+
+    public String getTienda() {
+        return tienda;
+    }
+
+    public void setTienda(String tienda) {
+        this.tienda = tienda;
+    }
+
+    public String getNumeroBoleta() {
+        return numeroBoleta;
+    }
+
+    public void setNumeroBoleta(String numeroBoleta) {
+        this.numeroBoleta = numeroBoleta;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
