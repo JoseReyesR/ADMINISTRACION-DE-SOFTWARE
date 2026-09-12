91af9857ae434dd8f07d9670dd156e783005569e
JoseReyesR <Josereyesrubio95@gmail.com>
2026-09-11 19:39:03 -0500

    BACKEND: MODIFICACION DE PROPIEDADES

--- reclamos-backend/src/main/java/reclamostottus/reclamos_backend/service/ReclamoService.java
+++ reclamos-backend/src/main/java/reclamostottus/reclamos_backend/service/ReclamoService.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import reclamostottus.reclamos_backend.dto.ReclamoRequestDTO;
+import reclamostottus.reclamos_backend.model.Categoria;
+import reclamostottus.reclamos_backend.model.EstadoReclamo;
+import reclamostottus.reclamos_backend.model.Prioridad;
 import reclamostottus.reclamos_backend.model.Reclamo;
+import reclamostottus.reclamos_backend.model.Usuario;
 import reclamostottus.reclamos_backend.repository.ReclamoRepository;
 
 import java.time.LocalDateTime;
@@ -57,6 +61,29 @@ public class ReclamoService {
          * Aquí asumimos que ya las buscaste previamente con sus repositorios.
          * Ejemplo: nuevoReclamo.setUsuario(usuarioRepository.findById(1).get());
          */
+        // ASIGNACIÓN TEMPORAL DE LLAVES FORÁNEAS (Para pruebas)
+        // En un flujo real, estos IDs vendrían de buscar el correo o la categoría en la
+        // BD
+
+        // Asignamos al usuario "Invitado" que creamos con ID 1 en el script SQL
+        Usuario usuarioInvitado = new Usuario();
+        usuarioInvitado.setId(1);
+        nuevoReclamo.setUsuario(usuarioInvitado);
+
+        // Asignamos Categoría 1 (Precio y Cobro)
+        Categoria catDefault = new Categoria();
+        catDefault.setId(1);
+        nuevoReclamo.setCategoria(catDefault);
+
+        // Asignamos Prioridad 2 (Media)
+        Prioridad prioDefault = new Prioridad();
+        prioDefault.setId(2);
+        nuevoReclamo.setPrioridad(prioDefault);
+
+        // Asignamos Estado 1 (Ingresado)
+        EstadoReclamo estadoDefault = new EstadoReclamo();
+        estadoDefault.setId(1);
+        nuevoReclamo.setEstado(estadoDefault);
 
         // 4. Guardar en Base de Datos
         return reclamoRepository.save(nuevoReclamo);
